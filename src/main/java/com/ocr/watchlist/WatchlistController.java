4d3557a5e29363b5b420c8dd262ad36b08e34eeb
imadealmorabit <imadealmorabit@gmail.com>
2020-05-17 00:34:05 +0000

    creation de l'entitee WatchlistItem et sa list dans le controleur

--- src/main/java/com/ocr/watchlist/WatchlistController.java
+++ src/main/java/com/ocr/watchlist/WatchlistController.java
@@ -4,20 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class WatchlistController {
 
+    private List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
+    private static int index = 1;
+
     @GetMapping("/watchlist")
     public ModelAndView getWatchlist(){
+
+        watchlistItems.clear();
+        watchlistItems.add(new WatchlistItem(index++, "Lion King","8.5","high","Hakuna matata!"));
+        watchlistItems.add(new WatchlistItem(index++, "Frozen","7.5","medium","Let it go!"));
+        watchlistItems.add(new WatchlistItem(index++, "Cars","7.1","low","Go go go!"));
+        watchlistItems.add(new WatchlistItem(index++, "Wall-E","8.4","high","You are crying!"));
 
         String viewName = "watchlist";
 
         Map<String,Object> model = new HashMap<>();
 
-        model.put("numberOfMovies", "1234");
+        model.put("watchlistItems", watchlistItems);
+        model.put("numberOfMovies", watchlistItems.size());
 
         return new ModelAndView(viewName,model);
     }
